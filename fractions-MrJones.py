4069875e0f004a649f612bd8972c708936c2ced7
Paul Jones <paul.tyler.jones@gmail.com>
2025-12-09 07:48:46 +0000

    Updates fraction script

--- fractions-MrJones.py
+++ fractions-MrJones.py
@@ -17,3 +17,18 @@ f4 = Fraction("7/8")  # Creates 7/8
 f5 = Fraction("0.25")  # Creates 1/4 (simplified!)
 print(f4)
 print(f5)
+
+# Create a fraction from the string "3/10"
+f6 = Fraction("3/10")
+
+# Create a fraction from the decimal string "0.5"
+f7 = Fraction("0.5")
+
+#Method 4: From a Float (Use Carefully!)
+f8 = Fraction(0.75)  # Creates 3/4
+print(f8)
+
+numerator = int(input("Enter numerator: "))
+denominator = int(input("Enter denominator: "))
+f9 = Fraction(numerator, denominator)
+print(f"Your fraction: {f9}")
